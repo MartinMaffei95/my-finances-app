98476ef86af4d5420b92e410121330e431f1fbcd
Martin Maffei <martinmaffei95@gmail.com>
2024-06-21 13:32:44 -0300

    feat. Basic config for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,64 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path";
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
+
+
+const pwaConfig:Partial<VitePWAOptions> ={
+  strategies: 'generateSW',
+  // when using strategies 'injectManifest' you need to provide the srcDir
+  // srcDir: 'src',
+  // when using strategies 'injectManifest' use claims-sw.ts or prompt-sw.ts
+  // filename: 'prompt-sw.ts',
+  registerType: 'prompt',
+  injectRegister: false,
+  pwaAssets: { disabled: false, config: true, htmlPreset: '2023', overrideManifestIcons: true },
+  manifest: {
+    name: 'My finances',
+    short_name: 'My finances',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: 'pwa-64x64.png',
+        sizes: '64x64',
+        type: 'image/png'
+      },
+      {
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any'  
+      },
+      {
+        src: 'maskable-icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable'
+      }
+    ]
+  },
+  workbox: {
+    globPatterns: ['**/*.{js,css,html,svg,png,svg,ico}'],
+    cleanupOutdatedCaches: true,
+    clientsClaim: true,
+  },
+  injectManifest: {
+    globPatterns: ['**/*.{js,css,html,svg,png,svg,ico}'],
+  },
+  devOptions: {
+    enabled: true,
+    navigateFallback: 'index.html',
+    suppressWarnings: true,
+    /* when using generateSW the PWA plugin will switch to classic */
+    type: 'module',
+  },
+}
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,6 +76,11 @@ export default defineConfig({
       "@/": `${path.resolve(__dirname, "src")}/`,
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA(pwaConfig)
+  ],
 
 })
+
+
